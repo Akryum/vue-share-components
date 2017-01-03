880b0f7b17110f240fad21d9b2f13e27a3174d23
Guillaume Chau <guillaume.b.chau@gmail.com>
2017-01-03 22:36:52 +0100

    common requires

--- template/config/webpack.config.common.js
+++ template/config/webpack.config.common.js
@@ -8,6 +8,7 @@ module.exports = merge(base, {
     filename: outputFile + '.common.js',
     libraryTarget: 'commonjs2',
   },
+  target: 'node',
   externals: {
     // Put external libraries like lodash here
     // With their package name
